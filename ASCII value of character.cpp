144adb6c692178fa46090d9706e72c7b70875f8b
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-13 22:44:19 +0530

    ASCII value of a character

---
+++ ASCII value of character.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c ='A'; 
+    cout << "The ASCII value of character '"<<c<<"' is: ";
+    int ascii = (int)c;
+    cout << ascii << endl;
+    
+    return 0;
+
+}
