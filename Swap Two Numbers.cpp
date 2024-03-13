ec502a1a50d3269f1fb3849459b54ed466546d79
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-03-13 23:19:40 +0530

    Swap Two Numbers

---
+++ Swap Two Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 2, b = 3;
+    cout << "Before swapping a = "<<a<<" and b = "<<b
+    << endl;
+    
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swapping a = "<<a<<" and b = "<<b
+    << endl;
+    return 0;
+}
