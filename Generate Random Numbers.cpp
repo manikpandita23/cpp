2f8975c380b433af45e7b08be36ee3e6c93371b9
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-04-07 00:18:31 +0530

    Generate Random Numbers

---
+++ Generate Random Numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    int i;
+    int num;
+    cout << "Generating random numbers:\n\n";
+    for (i = 1; i <=10;i++)
+    {
+        num=rand()%100;
+        cout<<" "<<num<<"  ";
+    }
+    cout << "\n";
+
+    return 0;
+}
